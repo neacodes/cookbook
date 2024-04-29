7913a27694a477c56eb629d6e206d2a28d2ef689
tonnitommi <tommi.holmgren@gmail.com>
2024-04-29 10:29:50 +0300

    added initial content

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# cookbook
-Inspiration and blueprints for AI Agents and Actions that define how knowledge work is done.
+# Welcome to Sema4.ai Cookbook! 🌟
+
+## What's This All About?
+
+The `cookbook` is a collection of examples, templates, and tutorials
+designed to showcase the possibilities of the Sema4.ai AI Agents and [Actions](https://github.com/Sema4AI/actions).
+
+## Project Status: Developer Preview
+
+It's important to note that the Sema4.ai Action Server itself is in an open-source developer preview phase.
+This means while you explore and contribute to the `cookbook`, you're also interacting with a platform
+that's actively evolving.
+
+## Explore Our Examples
+
+To get started, here are some of the basic examples:
+
+### Agents
+Coming soon!
+
+### Actions
+- [Simple greeter, think of Hello World](/actions/simple-greeter)
+- [Use API to get jokes](/actions/api-jokes)
+- [Automate browser to make Google searches](/actions/browser-google)
+- [Read data from a local Excel file, and write data back](/actions/excel-local)
+- [Expose Postgres database queries to AI app](/database-postgres/)
+
+## How to Contribute
+
+Ready to add your flavor to the cookbook? Here’s how:
+
+1. **Fork the Repository**: Start by creating your own fork. This is your canvas.
+2. **Create Magic**: Build your AI Agent blueprints or Actions. Whether it's a simple script or a complex algorithm, if it's innovative, it belongs here.
+3. **Submit a Pull Request**: Done with your masterpiece? Send us a PR, and let's review it together.
+
+Don't worry about the size or scope of your contribution. Every bit adds value!
+
+## Getting Started with Sema4.ai?
+
+New to Sema4.ai? No problem! We've got you covered with some helpful resources to get you started.
+Head over to our main [repo](https://github.com/Sema4AI/actions), and remember to leave a ⭐️.
+
+## Need Help?
+Questions? Hurdles? Ideas? Reach out by opening an issue, or join our [community discussions](https://github.com/robocorp/robocorp/discussions). We're here to support each other in this exciting AI adventure!
+
+## Acknowledgments
+A heartfelt thank you to all the trailblazers and supporters in our community. Your contributions, big or small, are the secret ingredients that make this cookbook sizzle!
---
+++ actions/huggingface-donut/actions.py
@@ -0,0 +1,68 @@
+"""
+Read receipts to JSON with Donut model from HuggingFace
+"""
+
+from sema4ai.actions import action
+from typing import Annotated
+from pydantic import BaseModel, Field
+
+import re
+from transformers import DonutProcessor, VisionEncoderDecoderModel
+import torch
+import requests
+from PIL import Image
+from io import BytesIO
+
+class ReceiptData(BaseModel):
+    receipt: Annotated[dict, Field(description="Extracted receipt contents")]
+
+@action(is_consequential=False)
+def read_and_extract_receipt_from_url(receipt_url: str) -> ReceiptData:
+    """
+    Converts a picture of an invoice to a structured data 
+
+    Args:
+        receipt_url (str): A complete URL that points to a one image file of a receipt.
+ 
+    Returns:
+        ReceiptData: A JSON representation of a receipts content
+    """
+
+    #receipt_url = "https://c8.alamy.com/comp/CNTYDX/tesco-shopping-receipt-CNTYDX.jpg"
+    response = requests.get(receipt_url)
+    if response.status_code == 200:
+        # Open the image directly from the bytes
+        image = Image.open(BytesIO(response.content))
+    
+    else:
+        return f"There was an error fetching image, status code: {response.status_code}"
+
+    processor = DonutProcessor.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2")
+    model = VisionEncoderDecoderModel.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2")
+    
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+
+    task_prompt = "<s_cord-v2>"
+    decoder_input_ids = processor.tokenizer(task_prompt, add_special_tokens=False, return_tensors="pt").input_ids
+    pixel_values = processor(image, return_tensors="pt").pixel_values
+
+    outputs = model.generate(
+        pixel_values.to(device),
+        decoder_input_ids=decoder_input_ids.to(device),
+        max_length=model.decoder.config.max_position_embeddings,
+        pad_token_id=processor.tokenizer.pad_token_id,
+        eos_token_id=processor.tokenizer.eos_token_id,
+        use_cache=True,
+        bad_words_ids=[[processor.tokenizer.unk_token_id]],
+        return_dict_in_generate=True,
+    )
+
+    sequence = processor.batch_decode(outputs.sequences)[0]
+    sequence = sequence.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
+    sequence = re.sub(r"<.*?>", "", sequence, count=1).strip()  # remove first task start token
+    print(processor.token2json(sequence))
+
+
+    return {"receipt": processor.token2json(sequence)}
+
